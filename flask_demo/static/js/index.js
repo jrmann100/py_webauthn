43ab81e89de5f0be72afde03311b77da903eead6
Jordan Mann <jordaloo@gmail.com>
2019-04-22 04:08:40 +0000

    Updated index to JSFiddle 8yrxf2kL/247.

--- flask_demo/static/js/index.js
+++ flask_demo/static/js/index.js
@@ -1,11 +1,37 @@
 setTimeout(() => {
   document.querySelectorAll("#card").forEach((x, i) => {
     x.classList.add("visible");
-    x.addEventListener('click', () => {
-      window.location = ["/ssh", "/account"][i]
-    }, false);
+    x.correspondingService = document.querySelectorAll("#iframe-wrapper")[i];
+    x.addEventListener('click', showService, false);
   })
 }, 100)
+
+outsideWrapper = function(evt) {
+  var a = evt.currentTarget;
+  var parents = [];
+  while (a) {
+    parents.unshift(a);
+    a = a.parentNode;
+  }
+  if (parents.includes(evt.currentTarget)){
+  	hideService();
+    document.removeEventListener('click', outsideWrapper);
+  }
+}
+
+showService = function(event) {
+  event.currentTarget.correspondingService.classList.add("visible");
+  document.querySelector("#exit").classList.add("visible");
+  activeService = event.currentTarget.correspondingService;
+  setTimeout(()=>{document.addEventListener('click', outsideWrapper)}, 100) // FIND BETTER SOLUTION
+}
+
+hideService = function() {
+  activeService.classList.remove("visible");
+  document.querySelector("#exit").classList.remove("visible");
+}
+
+//document.querySelector("#exit").addEventListener('click', hideService, false); // Since outsideWrapper() includes the button!
 
 document.querySelector("#logout").addEventListener('click', () => {
   window.location = "/logout"
